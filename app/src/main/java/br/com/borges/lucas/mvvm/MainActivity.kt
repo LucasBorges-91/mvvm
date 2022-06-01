676e1f2a4b6c3ff01e3a1bb6204a9136a7b64957
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-06-01 18:41:44 -0300

    finalizando o projeto criando a tela e o recyclerview e adicionando os observable na mainActivity.

--- app/src/main/java/br/com/borges/lucas/mvvm/MainActivity.kt
+++ app/src/main/java/br/com/borges/lucas/mvvm/MainActivity.kt
@@ -1,11 +1,67 @@
 package br.com.borges.lucas.mvvm
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import br.com.borges.lucas.mvvm.adapter.MainAdapter
+import br.com.borges.lucas.mvvm.databinding.ActivityMainBinding
+import br.com.borges.lucas.mvvm.repository.MainRepository
+import br.com.borges.lucas.mvvm.rest.RetrofitService
+import br.com.borges.lucas.mvvm.viewmodel.main.MainViewModel
+import br.com.borges.lucas.mvvm.viewmodel.main.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
+  lateinit var viewModel: MainViewModel
+
+  private val retrofitService = RetrofitService.getInstance()
+
+  private val adapter = MainAdapter { live ->
+    openLink(live.link)
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    viewModel = ViewModelProvider(
+      this,
+      MainViewModelFactory(MainRepository(retrofitService))
+    ).get(
+      MainViewModel::class.java
+    )
+
+    binding.recyclerview.adapter = adapter
+
+  }
+
+  override fun onStart() {
+    super.onStart()
+    viewModel.liveList.observe(this, Observer { lives ->
+      Log.i("Lucas", "onStart")
+      adapter.setLiveList(lives)
+    })
+
+    viewModel.errorMessage.observe(this, Observer {
+      Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+    })
+  }
+
+  override fun onResume() {
+    super.onResume()
+    viewModel.getAllLives()
+  }
+
+  private fun openLink(link: String) {
+    val browserIntent = Intent( Intent.ACTION_VIEW, Uri.parse(link))
+    startActivity(browserIntent)
   }
 }
